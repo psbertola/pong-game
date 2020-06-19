08daa0a0bc510bccbb4aba90f716edcbfefb527a
psbertola <66440385+psbertola@users.noreply.github.com>
2020-06-19 11:44:34 -0600

    Add files via upload

--- Game1.py
+++ Game1.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#Score
+score_a = 0
+score_b = 0
+
 #Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -34,6 +38,15 @@ ball.goto(0, 0)
 ball.dx = 1
 ball.dy = -1
 
+#Pen
+pen= turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align= "center", font= ("Courier", 18, "normal"))
+
 #Function
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -54,6 +67,7 @@ def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
     paddle_b.sety(y)
+
 #Keyboard binding 
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
@@ -82,16 +96,23 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1 
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align= "center", font= ("Courier", 18, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-    
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align= "center", font= ("Courier", 18, "normal"))
+        
+# Paddle and ball Collision    
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(340)
         ball.dx *= -1
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(-340)
         ball.dx *= -1
 
